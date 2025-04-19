7ddadd71f490355c847a706a425968a96f182281
Santyguz77 <Santyguz77@gmail.com>
2025-04-18 19:42:39 -0500

    primera subida

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector(".btn");
+    button.addEventListener("click", () => {
+        window.location.href = "intro.html";
+    });
+});
